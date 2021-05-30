bb95ee51de146fa002f69f8e923e49a18bce741c
AfrizalSA <71804747+AfrizalSA@users.noreply.github.com>
2021-05-30 19:56:26 +0200

    Update

--- Afrizal/Absensi_MA/resources/views/livewire/jabatans.blade.php
+++ Afrizal/Absensi_MA/resources/views/livewire/jabatans.blade.php
@@ -5,16 +5,14 @@
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
-            @if (session()->has('message'))
-                <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3" role="alert">
-                    <div class="flex">
-                        <div>
-                            <p class="text-sm">{{ session('message') }}</p>
-                        </div>
-                    </div>
-                </div>
+        @if(session()->has('message'))
+            <div class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded my-3">
+                 <div>
+                     <h1 class="text-black font-bold">{{ session('message') }}</h1>
+                 </div>
+            </div>
+            
             @endif
-
             
             <button wire:click="create()" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded my-3"> + Tambah Jabatan</button>
             
