9f0c0c3086ad6764bacfd8983e08baa44ce5ece9
AfrizalSA <71804747+AfrizalSA@users.noreply.github.com>
2021-06-08 18:11:26 +0200

    Update create_anggota.blade.php

--- Pembagian/Afrizal/Absensi_MA/resources/views/livewire/create_anggota.blade.php
+++ Pembagian/Afrizal/Absensi_MA/resources/views/livewire/create_anggota.blade.php
@@ -19,6 +19,7 @@
                         <div class="mb-4">
                             <label for="forJabatan" class="block text-gray-700 text-sm font-bold mb-2">Jabatan</label>
                             <select class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="forJabatan" name="Jabatan" wire:model="Jabatan">
+                            <option value="">- Pilih Jabatan -</option>
                             @foreach($Jabatans as $pangkat)
                             <option value="{{$pangkat->Jabatan}}">{{$pangkat->Jabatan}}</option>
                             @endforeach
